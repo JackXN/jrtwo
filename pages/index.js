0db6d685693d9ada54db97e5ef387584d23540ac
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-12-18 07:28:12 -0700

    fav

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,10 @@ useEffect(() => {
     <title>Jack Rigan</title>
     <meta name='description' content='Jack Rigan is a fullstack developer that enjoys'/>
     <meta property='og:title' content='Full-Stack Web Developer'/>  
+    <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
     
 {/* AOS */}
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
